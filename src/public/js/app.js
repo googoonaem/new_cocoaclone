59825f70c38c58c30b9cfc9900f3d65ee3032883
googool <wachmaninoff@gmail.com>
2023-09-07 10:40:38 +0900

    frontend setup
    nodemon ignore does not working.

---
+++ src/public/js/app.js
@@ -0,0 +1,1 @@
+console.log("h11i1");
--- src/server.js
+++ src/server.js
@@ -5,4 +5,11 @@ const app = express();
 const PORT = 3000;
 const listenHandler = () => console.log(`i'm listening from http://localhost:${PORT}!!`);
 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/",(req, res) => res.render("home"));
+
 app.listen(PORT, listenHandler);
